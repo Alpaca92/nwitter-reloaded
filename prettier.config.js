ad934f3084646b0e78b9d5de8b4d684a770c1241
Alpaca92 <zxcvbnm5288@gmail.com>
2023-09-25 21:03:27 +0900

    #1.1 Routing

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+};
+
+export default config;
