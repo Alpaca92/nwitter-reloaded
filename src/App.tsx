d6af39e90cc4f9bf4e5dee91f633e557f76f7d87
ayaan <ayaan@t-win.kr>
2023-09-26 10:33:07 +0900

    #2.1 Forms and UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Home from './routes/Home';
 import Profile from './routes/Profile';
 import Login from './routes/Login';
 import CreateAccount from './routes/Create-account';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { useState, useEffect } from 'react';
 import LoadingScreen from './components/Loading-screen';
-import { auth } from "./firebase";
+import { auth } from './firebase';
 
 const router = createBrowserRouter([
   {
@@ -49,6 +49,12 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const init = async () => {
@@ -61,10 +67,10 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 
